ea5b246193db8cb4657e8c8f68c65763b7ea613b
choiyongseon <cys71315@gmail.com>
2025-04-24 11:57:29 +0900

    Add files via upload
    2025.04.24 수정

---
+++ 5NP-main/OnlytestTRPG/OnlytestTRPG/MonsterManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlytestTRPG
+{
+    class MonsterManager
+    {
+        public static List<Monster> GenerateMonsters()
+        {
+            return new List<Monster>
+            {
+                new Monster("독고벌레",1, 20, 8, 20),
+                new Monster("바위게", 2, 25, 9, 10),
+                new Monster("칼날구울", 2, 15, 13, 10),
+                new Monster("늑대 고블린", 2, 20, 25, 10),
+                new Monster("붉은 도마뱀", 2, 30, 15, 50),
+                new Monster("쌍둥이 골렘", 2, 100, 10, 50),
+                new Monster("던전 악령", 2, 150, 40, 50),
+                new Monster("황금 드래곤", 20, 200, 50, 10000),
+            };
+        }
+
+        public static List<Monster> GetMonstersByStage(int stage)
+        {
+            return GenerateMonsters().FindAll(m => stage >= m.AppearStage);
+        }
+    }
+}
---
+++ 5NP-main/OnlytestTRPG/OnlytestTRPG/Status.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlytestTRPG; //나만 추가
+
+
+
+namespace OnlytestTRPG
+{
+    public class Status
+    {
+
+        public int nowEquipSTR;
+        public int nowEquipDEF;
+        public int nowEquipCRT;     //float으로 해야하나? 치명타 .퍼센트?
+        public int nowEquipAVD;    //회피율
+        public int nowEquipHP;
+        public int basicAVD = 10;
+        public int level = 1;
+        public string job = "전사";
+        public int basicCRT = 10;
+        public int basicSTR = 5;             //기본 공격력
+        public int basicDEF = 3;             //기본 방어력
+        public int basicHP = 100;            //기본 체력
+        public int basicGold = 1500;         //기본 소지금
+        public int CurrentHP = 100;
+    }
+
+}
